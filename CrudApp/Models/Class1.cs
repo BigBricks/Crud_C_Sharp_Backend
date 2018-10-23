c6ab3f01c847e192a60461fd14ed91a0dea465e7
BigBricks <BSA6.23.94@gmail.com>
2018-10-23 12:57:05 -0400

    working on solution

---
+++ CrudApp/Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudApp.Models
+{
+    public class Todo
+    {
+        public long id { get; set; }
+        public string Title { get; set; }
+        public bool Complete { get; set; }
+    }
+}
